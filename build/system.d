a9823c519eaa72eae089d17efe3557ebb7b5b4f4
Weston Hanners <weston.hanners@ymedialabs.com>
2017-12-01 08:39:23 -0500

    Remove build artifacts.

--- build/system.d
+++
@@ -1,4 +0,0 @@
-system.o: /Users/whanners/Desktop/GBA/source/system.c \
- /Users/whanners/Desktop/GBA/source/system.h
-
-/Users/whanners/Desktop/GBA/source/system.h:
--- build/test.d
+++
@@ -1,7 +0,0 @@
-test.o: /Users/whanners/Desktop/GBA/source/test.c \
- /Users/whanners/Desktop/GBA/source/system.h \
- /Users/whanners/Desktop/GBA/build/dubya.h
-
-/Users/whanners/Desktop/GBA/source/system.h:
-
-/Users/whanners/Desktop/GBA/build/dubya.h:
